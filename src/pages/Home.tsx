605aa988c2fd185b516ee766075fcb0718ab2a5f
Gavin Augsburger <gaugsburger45@gmail.com>
2024-07-16 19:03:08 -0500

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,8 @@ export default function Home() {
               <img src={Logo} alt="ER Automation Logo"></img>
             </Link>
             <h2 className="mt-3 text-center font-Merriweather text-base text-black md:mt-5">
-              Empowering Efficiency with Cutting-Edge Automation Solutions
+              TEST Empowering Efficiency with Cutting-Edge Automation
+              Solutions
             </h2>
           </div>
 
